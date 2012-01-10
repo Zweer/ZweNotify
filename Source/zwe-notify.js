c0f066d0e94614dd1c108090db33d3d9596e4c0c
Niccolo' Olivieri <flicofloc@gmail.com>
2012-01-10 14:20:26 +0100

    Completed (I hope)

--- Source/zwe-notify.js
+++ Source/zwe-notify.js
@@ -18,12 +18,12 @@ var ZweNotify = new Class({
         position: 'bottomRight',        // Possible values: 'bottomRight', 'bottomLeft', 'topRight', 'topLeft'
 
         show: {
-            duration: 500,
+            duration: 1000,
             transition: Fx.Transitions.Back.easeOut
         },
 
         close: {
-            duration: 500,
+            duration: 1000,
             transition: Fx.Transitions.Sine.easeOut
         },
 
@@ -47,6 +47,7 @@ var ZweNotify = new Class({
         this.options.position = typeOf(this.options.position) == 'string' ? this.positions[this.options.position] : this.options.position;
 
         this._createContainer();
+        this._createFx();
     },
 
     _createContainer: function() {
@@ -54,46 +55,69 @@ var ZweNotify = new Class({
         this.container.setStyles(this.options.position).setStyle('position', 'fixed');
     }.protect(),
 
+    _createFx: function() {
+
+    }.protect(),
+
     alert: function(message, options) {
+        options = Object.merge({}, this.options, options);
         if(typeOf(message) == 'string')
             message = { message: message, title: null, image: null };
 
         var current = this._createElement(message, options),
             closeEvent = function(event) {
                 event.preventDefault();
-                this._close(current);
+                this._close(current, options);
             }.bind(this);
 
         current.addEvent('rightClick', closeEvent);
-        current.getElement('a').addEvent('click', closeEvent);
+        current.getElement('a.' + options.name + '-close').addEvent('click', closeEvent);
 
         current.inject(this.container);
 
         this.elements.push(current);
 
-        this._show(message, options);
+        this._show(current, options);
     },
 
     _createElement: function(message, options) {
-        var move, container = new Element('div', { 'class': this.options.name + '-notification', id: String.uniqueID() }).adopt(move = new Element('div', { 'class': this.options.name + '-move' }).adopt(
-            new Element('a', { 'class': this.options.name + '-close' }).adopt(new Element('span', { text: this.options.localization.close })),
-            new Element('div', { 'class': this.options.name + '-message', html: message.message })
+        var container, move = new Element('div', { 'class': options.name + '-move', id: String.uniqueID() }).adopt(
+            container = new Element('div', {
+                'class': options.name + '-notification' + ' ' + options.name + '-fx-close'
+            }
+        ).addEvents({
+            mouseenter: function() {
+                container.morph('.' + options.name + '-fx-hover');
+                clearTimeout(move.retrieve('ZweNotify.CloseTimer', null));
+            },
+            mouseleave: function() {
+                this._close(move, options);
+            }.bind(this)
+        }).adopt(
+            new Element('a', { 'class': options.name + '-close' }).adopt(new Element('span', { text: options.localization.close })),
+            new Element('div', { 'class': options.name + '-message', html: message.message })
         ));
         if(message.title)
-            move.adopt(new Element('h3', { 'class': this.options.name + '-title', text: message.title }));
+            container.adopt(new Element('h3', { 'class': options.name + '-title', text: message.title }));
         if(message.image)
-            move.grab(new Element('img', { 'class': this.options.name + 'image', alt: 'image', src: message.image }), 'top');
+            container.grab(new Element('img', { 'class': options.name + 'image', alt: 'image', src: message.image }), 'top');
 
-        return container;
+        return move;
     }.protect(),
 
-    _show: function(message, options) {
+    _show: function(current, options) {
+        new Fx.Morph(current.getElement('.' + options.name + '-notification'), options.show).start('.' + options.name + '-fx-show');
 
+        current.store('ZweNotify.CloseTimer', this._close.delay(5000, this, [current, options]));
     }.protect(),
 
-    _close: function(element) {
-        this.elements.erase(element);
-        element.destroy();
+    _close: function(current, options) {
+        var container = current.getElement('.' + options.name + '-notification');
+        container.removeEvents('mouseenter').removeEvents('mouseleave');
+        new Fx.Morph(container, options.show).start('.' + options.name + '-fx-close').chain(function() {
+            this.elements.erase(current);
+            current.destroy();
+        }.bind(this));
     }
 });
 
